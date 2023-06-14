1e7f959a4fd7a87fe4a2203691eb01392ecce2e2
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-06-14 11:36:43 -0400

    Add code to try to get all possible rank 6 matrices for B_0. Brute force approach will not likely finish in reasonable amount of time.

--- virus.cpp
+++ virus.cpp
@@ -1,18 +1,18 @@
 //
 // Created by xavier on 6/13/2023.
 //
-#include <cstdio>
 #include <iostream>
 #include <vector>
 #include <Eigen/Dense>
 #include <Eigen/LU>
-#include <set>
+#include <chrono>
 
 using namespace std;
 
 typedef Eigen::Vector<float, 6> Vector6f;
 typedef Eigen::Matrix<float, 6, 6> Matrix6f;
 
+void append_vector(std::vector<Vector6f>&, std::vector<Vector6f>&, bool = true);
 std::vector<Vector6f> generateOrbit(const Vector6f&, std::vector<Matrix6f>&);
 Matrix6f ICO_centralizer(float, float);
 bool check_ICO_centralizer(Matrix6f);
@@ -111,14 +111,100 @@ int main() {
     f << 1,0,0,-1,0,0;
     f *= 0.5;
 
-    std::vector<Vector6f> orbit_s, orbit_b, orbit_Dinvb, orbit_Dinvf;
+    std::vector<Vector6f> orbit_s, orbit_b, orbit_Dinvb, orbit_Dinvf, P_0;
     orbit_s = generateOrbit(s, ICO);
     orbit_b = generateOrbit(b, ICO);
     orbit_Dinvb = generateOrbit(D.inverse()*b, ICO);
     orbit_Dinvf = generateOrbit(D.inverse()*f, ICO);
 
+    // create P_0 by appending all the orbits together
+    append_vector(P_0, orbit_s);
+    append_vector(P_0, orbit_b);
+    append_vector(P_0, orbit_Dinvb);
+    append_vector(P_0, orbit_Dinvf);
+
     for (Vector6f v : orbit_s) {
         cout << v.transpose() << endl;
+    }
+
+    // TODO: add the picking of linearly independent matrices from orbits and P_0
+    std::vector<std::vector<Vector6f>> B0_choices;
+    B0_choices.push_back(orbit_s);
+    B0_choices.push_back(orbit_Dinvb);
+    B0_choices.push_back(orbit_Dinvf);
+    B0_choices.push_back(orbit_b);
+    B0_choices.push_back(P_0);
+    B0_choices.push_back(P_0);
+
+
+
+
+
+    long long totalB0matrices = 1;
+    for (const vector<Vector6f>& mv : B0_choices) {
+        totalB0matrices *= mv.size();
+    }
+    cout << totalB0matrices << endl;
+
+    std::vector<Matrix6f> rank6matrices;
+    Matrix6f m;
+    m.setZero();
+    int rank;
+    // this part sucks because 6 nested for loops, but it's the simplest way to approach this
+    auto start_time = std::chrono::high_resolution_clock::now();
+    cout << "Starting nested loops...\n";
+    for (int first = 0; first < B0_choices[0].size(); ++first) {
+        for (int second = 0; second < B0_choices[1].size(); ++second) {
+            for (int third = 0; third < B0_choices[2].size(); ++third) {
+                for (int fourth = 0; fourth < B0_choices[3].size(); ++fourth) {
+                    for (int fifth = 0; fifth < B0_choices[4].size(); ++fifth) {
+                        for (int sixth = 0; sixth < B0_choices[5].size(); ++sixth) {
+                            m << B0_choices[0][first], B0_choices[1][second], B0_choices[2][third], B0_choices[3][fourth], B0_choices[4][fifth], B0_choices[5][sixth];
+
+                            // check our matrix is of rank 6
+                            if (m.colPivHouseholderQr().rank() == 6) {
+                                rank6matrices.push_back(m);
+//                                if (rank6matrices.size() >= 100) {
+//                                    cout << first << "\t" << second << "\t" << third << "\t" << fourth << "\t" << fifth << "\t" << sixth << endl;
+//                                    goto doneloop;
+//                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+//    doneloop:
+    cout << "\nDone.\n";
+    auto current_time = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Loop ran for: " << (current_time - start_time).count()/1000000000.0 << " seconds" << std::endl;
+
+//    for (Matrix6f M : rank6matrices) {
+//        cout << M << endl << endl;
+//    }
+    cout << rank6matrices.size() << endl;
+
+
+
+
+    // TODO: add the picking of linearly independent matrices from orbits and P_1 (and make P_1)
+}
+
+// append the contents of v2 to v1, possibly checking for duplicates
+void append_vector(std::vector<Vector6f>& v1, std::vector<Vector6f>& v2, bool removeDups) {
+    for (const Vector6f &v : v2) {
+        // if bool is true only append if it is not a duplicate
+        if (removeDups) {
+            if (std::find(v1.begin(), v1.end(), v) == v1.end()) {
+                v1.push_back(v);
+            }
+        }
+        // otherwise just append without checking
+        else {
+            v1.push_back(v);
+        }
     }
 }
 
