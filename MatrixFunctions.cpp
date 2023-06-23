1c763c1d9656ba4774d27f37402e0a40f255d2f7
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-06-23 17:04:58 -0400

    Add namespace for utility functions used on Eigen matrices

---
+++ MatrixFunctions.cpp
@@ -0,0 +1,89 @@
+#include "MatrixFunctions.hpp"
+
+// anonymous namespace for private functions
+namespace {
+    bool FloatsAreApproxEqual(float x, float y) {
+        const float relative_difference_factor = 0.0001;    // 0.01%
+        const float greater_magnitude = std::max(std::fabs(x),std::fabs(y));
+
+        return std::fabs(x-y) < relative_difference_factor * greater_magnitude;
+    }
+
+    bool FloatIsApproxZero(float x) {
+        const float epsilon = 0.00001;
+        return std::fabs(x) < epsilon;
+    }
+}
+
+namespace MatrixFunctions {
+    void print_size(const Eigen::MatrixXf& b)
+    {
+        std::cout << "size (rows, cols): " << b.size() << " (" << b.rows()
+                  << ", " << b.cols() << ")" << std::endl;
+    }
+
+    std::vector<float> entriesOfMatrix(const Eigen::MatrixXf& m, bool remove_duplicates) {
+        std::vector<float> entries;
+        float curr;
+        for (size_t i = 0; i < m.size(); i++) {
+            curr = *(m.data()+i);
+            if (remove_duplicates) {
+                if (std::find(entries.begin(), entries.end(), curr) == entries.end()) {
+                    entries.push_back(curr);
+                }
+            }
+            else
+                entries.push_back(curr);
+        }
+        return entries;
+    }
+
+    bool entriesOfMatrixAreOfParticularValues(const Eigen::MatrixXf& matrix, const std::vector<float>& values) {
+        bool entry_is_valid;
+        for (int i = 0; i < matrix.rows(); ++i) {
+            for (int j = 0; j < matrix.cols(); ++j) {
+                entry_is_valid = false;
+
+                // check if it is any of the valid values
+                for (float val : values) {
+                    if (FloatsAreApproxEqual(matrix(i,j), val))
+                        entry_is_valid = true;
+                }
+
+                if (!entry_is_valid)
+                    return false;
+            }
+        }
+
+        // every entry is valid
+        return true;
+    }
+
+    void fixZeroEntries(Eigen::MatrixXf& matrix) {
+        for (int i = 0; i < matrix.rows(); ++i) {
+            for (int j = 0; j < matrix.cols(); ++j) {
+                if (FloatsAreApproxEqual(matrix(i,j), 0)) {
+                    matrix(i,j) = 0;
+                }
+            }
+        }
+    }
+
+    void fixZeroEntries(EigenType::Matrix6f& matrix) {
+        for (int i = 0; i < matrix.rows(); ++i) {
+            for (int j = 0; j < matrix.cols(); ++j) {
+                if (FloatIsApproxZero(matrix(i,j))) {
+                    matrix(i,j) = 0;
+                }
+            }
+        }
+    }
+
+    bool matrixIsScalarOfIdentity(const EigenType::Matrix6f& matrix) {
+        return (matrix * 1.0/matrix(0,0)).isApprox(EigenType::Matrix6f::Identity());
+    }
+
+    bool matrixIsPlusMinusIdentity(const EigenType::Matrix6f& matrix) {
+        return matrix.isApprox(EigenType::Matrix6f::Identity()) || (-1*matrix).isApprox(EigenType::Matrix6f::Identity());
+    }
+} // MatrixFunctions
---
+++ MatrixFunctions.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <Eigen/Core>
+
+#include "EigenTypes.hpp"
+
+#ifndef VIRUS_RESEARCH_MATRIXFUNCTIONS_HPP
+#define VIRUS_RESEARCH_MATRIXFUNCTIONS_HPP
+
+namespace MatrixFunctions {
+    void print_size(const Eigen::MatrixXf& b);
+    std::vector<float> entriesOfMatrix(const Eigen::MatrixXf& m, bool remove_duplicates = false);
+    bool entriesOfMatrixAreOfParticularValues(const Eigen::MatrixXf& matrix, const std::vector<float>& values);
+    void fixZeroEntries(Eigen::MatrixXf& matrix);
+    void fixZeroEntries(EigenType::Matrix6f& matrix);
+    bool matrixIsScalarOfIdentity(const EigenType::Matrix6f& matrix);
+    bool matrixIsPlusMinusIdentity(const EigenType::Matrix6f& matrix);
+} // MatrixFunctions
+
+#endif //VIRUS_RESEARCH_MATRIXFUNCTIONS_HPP
