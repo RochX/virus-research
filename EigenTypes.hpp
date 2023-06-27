ddb9d01aa265ca01ef651139e4444f1ac6e02bfb
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-06-27 12:06:47 -0400

    Add a format for tab indenting a matrix

--- EigenTypes.hpp
+++ EigenTypes.hpp
@@ -10,6 +10,7 @@ namespace EigenType {
     typedef Eigen::Vector<float, 6> Vector6f;
     typedef Eigen::Matrix<float, 6, 6> Matrix6f;
     static Eigen::IOFormat COMMA_SEP_VALS(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", ", ", "", "", "", "");
+    static Eigen::IOFormat TAB_INDENT(Eigen::StreamPrecision, 0, " ", "\n", "\t");
 }
 
 #endif //VIRUS_RESEARCH_MATRIXTYPE_H
