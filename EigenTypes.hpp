48a1d01cc6f0d5584f70b0faf7315f1b4396d6a4
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-06-28 10:52:10 -0400

    Add matrix type for 6x3 matrices

--- EigenTypes.hpp
+++ EigenTypes.hpp
@@ -9,6 +9,7 @@
 namespace EigenType {
     typedef Eigen::Vector<float, 6> Vector6f;
     typedef Eigen::Matrix<float, 6, 6> Matrix6f;
+    typedef Eigen::Matrix<float, 6, 3> Matrix6fx3f;
     static Eigen::IOFormat COMMA_SEP_VALS(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", ", ", "", "", "", "");
     static Eigen::IOFormat TAB_INDENT(Eigen::StreamPrecision, 0, " ", "\n", "\t");
 }
