64812310c10d6200c891df5121461c84cb305e73
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-06-30 13:22:54 -0400

    Update to be able to use any column number

--- printOutputResults.cpp
+++ printOutputResults.cpp
@@ -15,23 +15,21 @@ int main(int argc, char *argv[]) {
     std::string line;
     int b0_cols;
 
-    bool get_user_input = true;
-    if (get_user_input) {
-        std::cout << "Enter which virus to get results of:\n";
-        std::getline(std::cin, current_virus);
-        std::cout << "Enter which centralizer to get results of:\n";
-        std::getline(std::cin, centralizer_to_check);
-        std::cout << "Enter how many columns the B0 matrices will be made with (3 or 6): \n";
-        std::getline(std::cin, line);
-        b0_cols = std::stoi(line);
-    }
-    else {
-        current_virus = "1044-1127";
-        centralizer_to_check = "D_10";
+    std::cout << "Enter which virus to get results of:\n";
+    std::getline(std::cin, current_virus);
+    std::cout << "Enter which centralizer to get results of:\n";
+    std::getline(std::cin, centralizer_to_check);
+    std::cout << "Enter how many columns the B0 matrices will be made with: \n";
+    std::getline(std::cin, line);
+    b0_cols = std::stoi(line);
+
+    if (b0_cols < 0 || b0_cols > 6) {
+        std::cout << "Invalid column number input, aborting...\n";
+        return 0;
     }
 
 
-    std::cout << "Getting T and B0 matrices that worked for group " + centralizer_to_check + " on virus " + current_virus + "...\n";
+    std::cout << "Getting T and B0 matrices that worked for group " + centralizer_to_check + " on virus " + current_virus + " from file...\n";
     std::ifstream fin (curr_directory + current_virus + "_T_and_B0_pairs_" + centralizer_to_check + ".txt");
 
     if (!fin.is_open()) {
@@ -39,14 +37,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    switch (b0_cols) {
-        case 3:
-            outputResults::output3colB0(fin);
-            break;
-        case 6:
-            outputResults::output6colB0(fin);
-            break;
-        default:
-            std::cout << "Invalid B0 column input." << std::endl;
-    }
+    outputResults::outputXcolB0(fin, b0_cols);
+    fin.close();
+    std::cout << "Done with file.\n";
 }
